9bffb032a1c02cf18d08d824e9a296b8f3ff619a
GabrielAvilesR <gabrielaviles1995@gmail.com>
2017-05-09 01:33:15 -0500

    joserra es puto

--- create_templates.m
+++ create_templates.m
@@ -1,25 +1,25 @@
 %CREATE TEMPLATES
 %Letter
 path = 'Templates\';
-A=imread(path + 'A.bmp');B=imread(path + 'B.bmp');
-C=imread(path + 'C.bmp');D=imread(path + 'D.bmp');
-E=imread(path + 'E.bmp');F=imread(path + 'F.bmp');
-G=imread(path + 'G.bmp');H=imread(path + 'H.bmp');
-I=imread(path + 'I.bmp');J=imread(path + 'J.bmp');
-K=imread(path + 'K.bmp');L=imread(path + 'L.bmp');
-M=imread(path + 'M.bmp');N=imread(path + 'N.bmp');
-O=imread(path + 'O.bmp');P=imread(path + 'P.bmp');
-Q=imread(path + 'Q.bmp');R=imread(path + 'R.bmp');
-S=imread(path + 'S.bmp');T=imread(path + 'T.bmp');
-U=imread(path + 'U.bmp');V=imread(path + 'V.bmp');
-W=imread(path + 'W.bmp');X=imread(path + 'X.bmp');
-Y=imread(path + 'Y.bmp');Z=imread(path + 'Z.bmp');
+A=imread('Temp\A.bmp');B=imread('Temp\B.bmp');
+C=imread('Temp\C.bmp');D=imread('Temp\D.bmp');
+E=imread('Temp\E.bmp');F=imread('Temp\F.bmp');
+G=imread('Temp\G.bmp');H=imread('Temp\H.bmp');
+I=imread('Temp\I.bmp');J=imread('Temp\J.bmp');
+K=imread('Temp\K.bmp');L=imread('Temp\L.bmp');
+M=imread('Temp\M.bmp');N=imread('Temp\N.bmp');
+O=imread('Temp\O.bmp');P=imread('Temp\P.bmp');
+Q=imread('Temp\Q.bmp');R=imread('Temp\R.bmp');
+S=imread('Temp\S.bmp');T=imread('Temp\T.bmp');
+U=imread('Temp\U.bmp');V=imread('Temp\V.bmp');
+W=imread('Temp\W.bmp');X=imread('Temp\X.bmp');
+Y=imread('Temp\Y.bmp');Z=imread('Temp\Z.bmp');
 %Number
-one=imread(path + '1.bmp');  two=imread(path + '2.bmp');
-three=imread(path + '3.bmp');four=imread(path + '4.bmp');
-five=imread(path + '5.bmp'); six=imread(path + '6.bmp');
-seven=imread(path + '7.bmp');eight=imread(path + '8.bmp');
-nine=imread(path + '9.bmp'); zero=imread(path + '0.bmp');
+one=imread('Temp\1.bmp');  two=imread('Temp\2.bmp');
+three=imread('Temp\3.bmp');four=imread('Temp\4.bmp');
+five=imread('Temp\5.bmp'); six=imread('Temp\6.bmp');
+seven=imread('Temp\7.bmp');eight=imread('Temp\8.bmp');
+nine=imread('Temp\9.bmp'); zero=imread('Temp\0.bmp');
 %*-*-*-*-*-*-*-*-*-*-*-
 letter=[A B C D E F G H I J K L M...
     N O P Q R S T U V W X Y Z];
